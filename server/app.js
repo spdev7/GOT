540608827066332677c05571cde95d577f424c49
Aditya Dhawan <aditya.dhawan@popxo.com>
2020-05-10 21:04:04 +0530

    update route

--- server/app.js
+++ server/app.js
@@ -34,7 +34,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("/search", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/../build/index.html"));
   });
 
